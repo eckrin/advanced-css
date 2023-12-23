3af3c33ff4930ebc66d5654bf947b9b6b35c3182
eckrin <bsh51515@naver.com>
2023-12-23 20:25:17 +0900

    231223: add side navigation bar

--- main/shared.js
+++ main/shared.js
@@ -1,12 +1,32 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
-var selectPlanButton = document.querySelectorAll('.plan button');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 // console.log('backdrop');
-console.dir(selectPlanButton);
-for (var i=0; i<selectPlanButton.length; i++) {
-    selectPlanButton[i].addEventListener('click', function() {
+// console.dir(selectPlanButton);
+for (var i=0; i<selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
         modal.style.display = 'block';
         backdrop.style.display = 'block';
     });
 }
+
+backdrop.addEventListener("click", function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+}
+
+toggleButton.addEventListener("click", function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
